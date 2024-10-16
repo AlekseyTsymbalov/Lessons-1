7b3353b91c1eccd0663c9e5885aef4b7fb79adc8
Алексеус <AlekseyTsymbalov@yahoo.com>
2024-10-16 15:08:00 +0300

    Completed for verification

---
+++ A school riddle.py
@@ -0,0 +1,7 @@
+# // "3rd program"
+sum1 = (2 * 2 + 2)
+sum2 = (2 * (2 + 2))
+
+print(sum1)
+print(sum2)
+print(sum1 == sum2)
---
+++ The first one after the dot.py
@@ -0,0 +1,9 @@
+# // "4th program"
+N = '123.456'
+meaning = float(N)
+
+operation = meaning * 10
+meaning1 = int(operation)
+
+total = meaning1 % 10
+print(total)
