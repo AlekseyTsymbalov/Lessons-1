e4d727fc7f4a8da492accc87fce4e8d9ac5dac32
Алексеус <AlekseyTsymbalov@yahoo.com>
2024-10-18 22:43:57 +0300

    Completed for verification

---
+++ Variables.py
@@ -0,0 +1,5 @@
+completed_homework = 12
+hoursSpent = 1.5
+course = "Python"
+time_spent = hoursSpent / completed_homework
+print(f"Курс:{course}, всего задач:{completed_homework}, затрачено часов:{hoursSpent}, среднее время выполнения:{time_spent}")
