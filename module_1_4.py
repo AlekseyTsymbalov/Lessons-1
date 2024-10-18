f8722b94f755e0ab54557b08f87219832de54635
Алексеус <AlekseyTsymbalov@yahoo.com>
2024-10-18 23:03:00 +0300

    Completed for verification 18.10.2024

--- module_1_4.py
+++ module_1_4.py
@@ -1,1 +1,8 @@
-my_string = input("Через терни к звёздам!")
+my_string = "Через терни к звёздам!"
+print(my_string)
+print(len(my_string), "- Символа в строке выше")
+print(my_string.upper())
+print(my_string.lower())
+print("Через терни к звёздам!".replace(" ", ""))
+print(my_string[0])
+print(my_string[-1])
