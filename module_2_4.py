d08f694b4a22a8f1f9bff66fd6a021d339271ea1
Алексеус <AlekseyTsymbalov@yahoo.com>
2024-11-03 18:32:19 +0300

    Ready for verification 03.11.2024

---
+++ module_2_4.py
@@ -0,0 +1,23 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+
+for digits in numbers:
+    is_prime = True
+
+    if digits < 2:
+        is_prime = False
+
+    for  divider in range(2, int(digits ** 0.5) + 1):
+        if digits % divider == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        primes.append(digits)
+    else:
+        not_primes.append(digits)
+
+print(f"Исходный код:\nnumbers = {numbers}")
+print(f"Primes: {primes}")
+print(f"Not Primes: {not_primes}")
