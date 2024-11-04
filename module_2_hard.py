2cd18107317faec9c46662b3674fb42dada87883
Алексеус <AlekseyTsymbalov@yahoo.com>
2024-11-04 11:40:40 +0300

    Ready for verification 04.11.2024

---
+++ module_2_hard.py
@@ -0,0 +1,21 @@
+def generate_password(n):
+    result = ""
+
+    pairs = []
+
+    for i in range(1, n):
+        for j in range(i + 1, n + 1):
+            pairs.append((i, j))
+
+    for a, b in pairs:
+        pair_sum = a + b
+        if n % pair_sum == 0:
+            result += f"{a}{b}"
+
+    return result
+
+
+passwords = {n: generate_password(n) for n in range(3, 21)}
+
+for n in range(3, 21):
+    print(f"{n} - {passwords[n]}")
