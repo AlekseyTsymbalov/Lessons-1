9201901598f14a87af21d6707ec08893945d337f
Aleksey <AlekseyTsymbalov@yahoo.com>
2024-11-20 12:52:10 +0300

    Fix line 7

--- module_2_hard.py
+++ module_2_hard.py
@@ -4,7 +4,7 @@ def generate_password(n):
     pairs = []
 
     for i in range(1, n):
-        for j in range(i + 1, n):
+        for j in range(i + 1, n): # Fixed it
             pairs.append((i, j))
 
     for a, b in pairs:
