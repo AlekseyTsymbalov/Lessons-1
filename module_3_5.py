04d72912be526ca6beed580a2ac6e1e3989edba0
Aleksey <AlekseyTsymbalov@yahoo.com>
2024-11-21 20:09:38 +0300

    Completed for verification

--- module_3_5.py
+++ module_3_5.py
@@ -3,7 +3,6 @@ def get_multiplied_digits(number):
 
     if len(str_number) > 1:
         first = int(str_number[0])
-
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return int(str_number)
